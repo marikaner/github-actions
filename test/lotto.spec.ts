4a4785ff1f3e82a40e77e35ea3d1fed88cd53f5b
Junjie Tang <junjie.tang@sap.com>
2020-02-13 13:03:53 +0100

    add lint stage

--- test/lotto.spec.ts
+++ test/lotto.spec.ts
@@ -1,11 +1,11 @@
-import {getNewResult} from "../src/lotto";
+import { getNewResult } from '../src/lotto';
 
-describe("test lotto.ts", () => {
-    it("test length", async ()  => {
+describe('test lotto.ts', () => {
+    it('test length', async ()  => {
         expect(getNewResult()).toHaveLength(6);
     });
 
-    it("test random", async ()  => {
+    it('test random', async ()  => {
         expect(getNewResult()).not.toEqual(getNewResult());
     });
 });
