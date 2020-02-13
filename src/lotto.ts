0d14c131baa499b93366d1282be94cffe5854f02
Junjie Tang <junjie.tang@sap.com>
2020-02-13 12:32:21 +0100

    add test step

---
+++ src/lotto.ts
@@ -0,0 +1,15 @@
+
+main();
+
+function main(){
+    console.info('starting');
+    console.info(getNewResult());
+}
+
+export function getNewResult(): number[] {
+    return Array.from({length: 6}, () => getRandomInt(10));
+}
+
+function getRandomInt(max):number {
+    return Math.floor(Math.random() * Math.floor(max));
+}
---
+++ test/lotto.spec.ts
@@ -0,0 +1,11 @@
+import {getNewResult} from "../src/lotto";
+
+describe("test lotto.ts", () => {
+    it("test length", async ()  => {
+        expect(getNewResult()).toHaveLength(6);
+    });
+
+    it("test random", async ()  => {
+        expect(getNewResult()).not.toEqual(getNewResult());
+    });
+});
